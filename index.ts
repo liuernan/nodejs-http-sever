25a885143f76edfd94ee2bfe71cde76aca863ed5
Jiawei <liujiaweidreamer@gmail.com>
2020-07-25 22:46:30 +0800

    successfully start a simplest http serve

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('hello, TypeScript.');
+import * as http from "http";
+
+const server = http.createServer();
+
+server.on("request", (request, response) => {
+  console.log("someone request you");
+  // console.log("request:");
+  // console.log(request);
+  // console.log("response:");
+  // console.log(response);
+  response.end("hi, I received ur request.");
+});
+
+server.listen(6666);
