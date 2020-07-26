d58806d800aac539d05c7c9e71530334f6a51625
Jiawei <liujiaweidreamer@gmail.com>
2020-07-26 21:24:42 +0800

    看文档知道 request 是 IncomingMessage 类的实例，response 是 ServerResponse 类的实例，使用 TS 的类型声明，这样可以在写代码的时候，WebStorm 给出相应的自动提示

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import * as http from "http";
+import {IncomingMessage, ServerResponse} from "http";
 
 const server = http.createServer();
 
-server.on("request", (request, response) => {
-  console.log("someone request you");
-  // console.log("request:");
-  // console.log(request);
-  // console.log("response:");
-  // console.log(response);
+server.on("request", (request: IncomingMessage, response: ServerResponse) => {
+  console.log(request.url);
+  console.log(request.method);
+  console.log(request.headers);
+
   response.end("hi, I received ur request.");
 });
 
