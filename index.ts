fc88244492d3f07fdd6f607ffc766cade414d252
Jiawei <liujiaweidreamer@gmail.com>
2020-07-27 16:18:14 +0800

    引入 fs 和 path，可以把文件传回客户端

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as fs from "fs";
+import * as path from "path";
 
 const server = http.createServer();
 
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
-  console.log(request.url);
-  console.log(request.method);
-  console.log(request.headers);
+  // console.log(request.url);
+  // console.log(request.method);
+  // console.log(request.headers);
 
-  response.end("hi, I received ur request.");
+  const data = fs.readFileSync(path.join("public", "index.html"));
+  // response.write(data);
+  // response.setHeader("liuernan", "hahaha");
+  // response.statusCode = 404;
+  response.end(data);
 });
 
-server.listen(6666);
+server.listen(8080);
